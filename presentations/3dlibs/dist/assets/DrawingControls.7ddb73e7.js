da587a95d34b4b4011d3415ddace8184a4e1bc44
SpookyJelly <zang9412@gmail.com>
2022-11-25 15:05:45 +0900

    1.remove dist

--- presentations/3dlibs/dist/assets/DrawingControls.7ddb73e7.js
+++
@@ -1,1 +0,0 @@
-import"./DrawingControls.vue_vue_type_script_setup_true_lang.e73366c2.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.e73366c2.js";import"./index.fe95fcf9.js";export{t as default};
--- presentations/lab/dist/assets/DrawingPreview.b5a005b3.js
+++
@@ -1,1 +0,0 @@
-import"./index.8524371e.js";import{al as r}from"./index.8524371e.js";export{r as default};
