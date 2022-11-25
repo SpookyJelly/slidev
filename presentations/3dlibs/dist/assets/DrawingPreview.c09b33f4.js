b39b8af44023a4d2b6feb2e28bd9e7795639e14d
SpookyJelly <zang9412@gmail.com>
2022-11-25 15:00:16 +0900

    test: add dist forEach project.

---
+++ presentations/3dlibs/dist/assets/DrawingPreview.c09b33f4.js
@@ -0,0 +1,1 @@
+import"./index.fe95fcf9.js";import{am as r}from"./index.fe95fcf9.js";export{r as default};
---
+++ presentations/lab/dist/assets/DrawingPreview.b5a005b3.js
@@ -0,0 +1,1 @@
+import"./index.8524371e.js";import{al as r}from"./index.8524371e.js";export{r as default};
