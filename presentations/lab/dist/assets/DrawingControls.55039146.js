7f503bd140ba5f5bd7d655dbe2af842b30f6317b
SpookyJelly <zang9412@gmail.com>
2022-11-29 13:25:49 +0900

    add dist on gitignore && remove build of lab
    ** let's see what's happening

--- presentations/lab/dist/assets/DrawingControls.55039146.js
+++
@@ -1,1 +0,0 @@
-import"./DrawingControls.vue_vue_type_script_setup_true_lang.26aa8cc2.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.26aa8cc2.js";import"./index.9bf8bf60.js";export{t as default};
--- presentations/lab/dist/assets/DrawingPreview.ef60c809.js
+++
@@ -1,1 +0,0 @@
-import"./index.9bf8bf60.js";import{al as r}from"./index.9bf8bf60.js";export{r as default};
